d5a6e3c605c2880bbffb560d13dd99ae5228f867
Dálcio Garcia <dalciomacueegarcia@gmail.com>
2022-06-18 07:07:44 +0100

    feat: search task

--- src/components/TrackOptions/SearchTask.tsx
+++ src/components/TrackOptions/SearchTask.tsx
@@ -1,6 +1,16 @@
 import { TextInput } from '@mantine/core';
 import { MagnifyingGlassIcon } from '@modulz/radix-icons';
+import { useSearchTask } from '@store/tracker';
 
 export default function SearchTask() {
-  return <TextInput placeholder="Search tasks..." icon={<MagnifyingGlassIcon />} />;
+  const [name, setName] = useSearchTask();
+
+  return (
+    <TextInput
+      value={name}
+      onChange={(value) => setName(value.currentTarget.value)}
+      placeholder="Search tasks..."
+      icon={<MagnifyingGlassIcon />}
+    />
+  );
 }
--- src/store/tracker.ts
+++ src/store/tracker.ts
@@ -1,15 +1,45 @@
-import { useAtom } from 'jotai';
+import { TApp } from '@my-types/App.types';
+import { TStatus, TTracker } from '@my-types/Tracker.types';
+import { atom, useAtom } from 'jotai';
+import { appAtom } from './appState';
 
-import { useCurrentWorkspace, workspacesAtom } from './workspaces';
+import { currentWorkspaceAtom, useCurrentWorkspace, workspacesAtom } from './workspaces';
+
+const filterTasksAtom = atom('');
+
+const currentTrackerAtom = atom<TTracker | undefined>((get) => {
+  const app = get<TApp>(appAtom);
+  const idx = app.currentWorkspace[1];
+
+  if (idx === undefined) return undefined;
+  const currWorkspaceTracker = app.workspaces[idx].tracker;
+
+  const tracker = { ...currWorkspaceTracker };
+
+  tracker.backlog = currWorkspaceTracker.backlog.filter(({ name }) =>
+    name.toLowerCase().includes(get(filterTasksAtom))
+  );
+  tracker['to-do'] = currWorkspaceTracker['to-do'].filter(({ name }) =>
+    name.toLowerCase().includes(get(filterTasksAtom))
+  );
+  tracker.doing = currWorkspaceTracker.doing.filter(({ name }) =>
+    name.toLowerCase().includes(get(filterTasksAtom))
+  );
+  tracker.done = currWorkspaceTracker.done.filter(({ name }) =>
+    name.toLowerCase().includes(get(filterTasksAtom))
+  );
+
+  return tracker;
+});
+
+export const useSearchTask = () => useAtom(filterTasksAtom);
 
 export const useCurrentTracker = () => {
-  const [workspaces] = useAtom(workspacesAtom);
-  const [, currIdx] = useCurrentWorkspace();
+  const [tracker] = useAtom(currentTrackerAtom);
 
   const empty = { backlog: [], 'to-do': [], doing: [], done: [] };
 
   return {
-    currentTracker:
-      (currIdx !== undefined && currIdx >= 0 && (workspaces[currIdx]?.tracker ?? empty)) || empty,
+    currentTracker: tracker ?? empty,
   };
 };
