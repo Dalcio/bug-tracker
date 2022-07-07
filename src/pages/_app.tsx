2d78cc7c486b55ba1d2a7ba505681f2ee869f720
Dálcio Garcia <dalciomacueegarcia@gmail.com>
2022-07-07 18:42:32 +0100

    page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Bug Tracker</title>
+        <title>Task Tracker</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
